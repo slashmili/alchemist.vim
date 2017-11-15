553d7f478e3742cffd99416590452e72264b94b9
Milad Rastian <milad@high-mobility.com>
2017-11-15 19:39:39 +0100

    add nvim-completion-manager plugin
    it's an Async Completion for neovim

--- pythonx/cm_sources/alchemist.py
+++ pythonx/cm_sources/alchemist.py
@@ -1,45 +1,52 @@
+# -*- coding: utf-8 -*-
+# For debugging, use this command to start neovim:
+#
+# NVIM_PYTHON_LOG_FILE=nvim.log NVIM_PYTHON_LOG_LEVEL=INFO nvim
+
 import os, sys
-PLUGIN_BASE_PATH = os.path.abspath("%s/../../../../../" % __file__)
+PLUGIN_BASE_PATH = os.path.abspath("%s/../../../" % __file__)
 sys.path.insert(0, PLUGIN_BASE_PATH)
 from elixir_sense import ElixirSenseClient
 import re
-from .base import Base
 
 DEBUG = False
 
-class Source(Base):
-    def __init__(self, vim):
-        Base.__init__(self, vim)
+from cm import register_source, getLogger, Base
+register_source(name='alchemist',
+                priority=8,
+                abbreviation='ex',
+                scopes=['elixir'],
+                cm_refresh_patterns=[r'\.'],)
 
-        self.name = 'alchemist'
-        self.mark = '[alchemist]'
-        self.filetypes = ['elixir']
-        self.is_bytepos = False
+logger = getLogger(__name__)
+
+class Source(Base):
+    def __init__(self, nvim):
+        super(Source, self).__init__(nvim)
         self.re_suggestions = re.compile(r'kind:(?P<kind>.*), word:(?P<word>.*), abbr:(?P<abbr>.*), menu:(?P<menu>.*), info:(?P<info>.*)$')
         self.re_is_only_func = re.compile(r'^[a-z].*')
 
         alchemist_script = "%s/elixir_sense/run.exs" % PLUGIN_BASE_PATH
         self.sense_client = ElixirSenseClient(debug=DEBUG, cwd=os.getcwd(), ansi=False, elixir_sense_script=alchemist_script, elixir_otp_src="")
 
-    def get_complete_position(self, context):
-        return self.vim.call('elixircomplete#auto_complete', 1, '')
+    def cm_refresh(self, info, ctx):
+        lnum = self.nvim.funcs.line('.')
+        cnum = self.nvim.funcs.col('.')
+        lines = self.nvim.funcs.getline(1, '$')
+        complete_str = ctx['typed']
 
-    def gather_candidates(self, context):
-        lnum = self.vim.funcs.line('.')
-        cnum = self.vim.funcs.col('.')
-        lines = self.vim.funcs.getline(1, '$')
-        complete_str = context['complete_str']
 
         response = self.sense_client.process_command('suggestions', "\n".join(lines), lnum ,cnum)
         if response[0:6] == 'error:':
-            self.vim.command('echohl ErrorMsg|echom "%s"|echohl None' % response)
+            logger.error("================= alchemist error: %s =====", response)
+            self.nvim.command('echohl ErrorMsg|echom "%s"|echohl None' % response)
             return []
 
-        return self.__get_suggestions__(complete_str, response.split('\n')[:-1])
+        startcol = ctx['startcol'] - len(complete_str)
+        matches = self.__get_suggestions__(complete_str, response.split('\n')[:-1])
+        self.complete(info, ctx, startcol, matches)
 
-    # Private implementation
-    ########################
-
+        return matches
     def __get_suggestions__(self, complete_str, server_results):
         suggestions = []
         for result in server_results:
@@ -55,8 +62,8 @@ class Source(Base):
                 'abbr': matches.group('abbr'),
                 'menu': matches.group('menu'),
             }
-            if self.vim.funcs.exists('g:alchemist#extended_autocomplete'):
-                if self.vim.eval('g:alchemist#extended_autocomplete') == 1:
+            if self.nvim.funcs.exists('g:alchemist#extended_autocomplete'):
+                if self.nvim.eval('g:alchemist#extended_autocomplete') == 1:
                     sugg['info'] = matches.group('info').replace("<n>", "\n").strip()
             suggestions.append(sugg)
 
